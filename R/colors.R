cb80974a06d24c7955156dc763d817354263e4e6
Roger D. Peng [audrey] <rdpeng@gmail.com>
2017-10-26 10:49:53 +1100

    Create a separate file for color palettes

---
+++ R/colors.R
@@ -0,0 +1,21 @@
+## Australia Color Palettes
+
+## Albert Namatjira: Twin Ghosts
+## http://www.menziesartbrands.com/sites/default/files/field/catalogue_items//DM_24017.jpg
+namatjira <- c(
+    "#d8f0f0",	
+    "#483030",
+    "#784830",
+    "#786060",
+    "#d8c0a8",	
+    "#a86030",
+    "#a890a8",
+    "#486090",
+    "#787890",
+    "#a89078",
+    "#d8a890",
+    "#c07848",
+    "#d89048",
+    "#c09060",
+    "#f0a860"
+)
