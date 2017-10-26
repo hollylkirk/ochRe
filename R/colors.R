31c64d20e1da545278d5ff6f127a785ab41e924a
Dianne Cook <visnut@gmail.com>
2017-10-26 13:55:42 +1100

    ochre palettes function

--- R/colors.R
+++ R/colors.R
@@ -1,8 +1,10 @@
-## Australia Color Palettes
-
+#' Australia Color Palettes
+#'
+#' @export
+ochre_palettes <- list(
 ## Albert Namatjira: Twin Ghosts
 ## http://www.menziesartbrands.com/sites/default/files/field/catalogue_items//DM_24017.jpg
-namatjira <- c(
+  namatjira <- c(
     "#d8f0f0",	
     "#786060",
     "#d8c0a8",	
@@ -11,11 +13,11 @@ namatjira <- c(
     "#486090",
     "#a89078",
     "#f0a860"
-)
+  )
 
 ## Georgiana McCrae, "Melbourne, Collins St, 1841",
 ## https://en.wikipedia.org/wiki/File:Melbourne_Collins_Street_1841.jpg
-mccrea_melbcollinsst_1841 <- c(
+  mccrea_melbcollinsst_1841 <- c(
     "#a8c0a8",
     "#c0c0a8",
     "#a8a890",
@@ -27,11 +29,11 @@ mccrea_melbcollinsst_1841 <- c(
     "#c07848",
     "#d89060",
     "#c07848"
-)
+  )
     
 # based on the great all tapestry https://www.aph.gov.au/Visit_Parliament/Art/Top_5_Treasures/Great_Hall_Tapestry
 # and house of reps and senate colours https://www.peo.gov.au/learning/closer-look/australias-parliament-house/a-place-for-the-parliament.html
-parliament <- c(
+  parliament <- c(
     "#a84848", # senate red
     "#f0d8d8", # light pink/red
     "#304830", # dark green
@@ -40,12 +42,11 @@ parliament <- c(
     "#90a8a8", # light blue
     "#906030", # brown
     "#c0a890" # light brown
-)
-
+  )
 
 # based on photographs from cradle mountain national park and the bay of fires
 # random comment
-tasmania <- c(
+  tasmania <- c(
     "#a84830", # bay of fires lychen
     "#d89060", # lighter lychen
     "#604830", # forest brown
@@ -53,12 +54,11 @@ tasmania <- c(
     "#c0c0d8", # crater lake grey
     "#606030", # fern green
     "#486030" # enchanted walk moss
-)
-
+  )
 
 ## Sydney Nolan, "Ned Kelly 1946",
 ## https://cs.nga.gov.au/detail.cfm?irn=28926
-nolan_ned <- c(
+  nolan_ned <- c(
     "#a89030", # ochre
     "#004878", # sky blue
     "#78a890", # dusty blue
@@ -66,13 +66,9 @@ nolan_ned <- c(
     "#481800"  # brown
     )
 
-
-
-
-
 ## Wayne Ludbey, "Nicky Winmar St Kilda Footballer", 1993
 ## https://nga.gov.au/federation/Detail.cfm?WorkID=27707&ZoomID=3
-winmar <- c(
+  winmar <- c(
     "#d8d8d8",
     "#609060",
     "#183030",
@@ -80,20 +76,19 @@ winmar <- c(
     "#604848",
     "#d8c0a8",
     "#a86048"
-)
+  )
 
 ## John Olsen, "Sydney Sun, 1965",
 ## https://artsearch.nga.gov.au/Detail-LRG.cfm?IRN=26102
-olsen_qual <- c(
+  olsen_qual <- c(
     "#c47000", # rusty yellow
     "#e0c48c", # light tan
     "#8c3800", # dark orange
     "#e0a81c", # yellow
     "#8ca8c4", # dusky blue
     "#385438"  # dark green
-)
-
-olsen_seq <- c(
+  )
+  olsen_seq <- c(
     "#701c00", 
     "#8c3800", 
     "#a85400", 
@@ -108,12 +103,11 @@ olsen_seq <- c(
     "#1c54a8",
     "#385438",
     "#38381c"
-)
-
+  )
 ## Fred Williams, Mount Nameless (morning)
 ## http://www.art-almanac.com.au/wp-content/uploads/2013/11/benalla_mt_nameless_review_1213.jpg
 ## 7 30 y n
-williams_pilbara <- c(
+  williams_pilbara <- c(
     "#d23c1e", # 3
     "#965a3c", # 6
     "#b4783c", # 2
@@ -121,11 +115,10 @@ williams_pilbara <- c(
     "#96b4b4", # 7
     "#96b4d2", # 4
     "#7896b4"  # 1
-)
-
+  )
 ## Emily Kame Kngwarreye, Emu Woman 1988–89
 ## http://www.nma.gov.au/exhibitions/utopia_the_genius_of_emily_kame_kngwarreye/emily_kame_kngwarreye
-emu_woman_paired <- c(
+  emu_woman_paired <- c(
    "#AB7E37", 
    "#79222A",
    "#562323",
@@ -144,4 +137,5 @@ emu_woman_paired <- c(
    "#78292F",
    "#B6B0AE",
    "#3A3A43"
+  )
 )
