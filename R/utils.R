8668cb48ddbd14efa62a871ce75b491eab414e0c
Roger D. Peng [audrey] <rdpeng@gmail.com>
2017-10-26 11:47:27 +1100

    Add back viz_palette function!

---
+++ R/utils.R
@@ -0,0 +1,15 @@
+viz_palette <- function(pal) {
+    n <- length(pal)
+    plot(1:n, rep(0, n), xaxt = "n", yaxt = "n", 
+         col = pal, pch = 19, cex = 5)
+    text(1:n, rep(0.5, n), pal, srt = 90)
+}
+
+order_palette <- function(pal) {
+    r <- col2rgb(pal) %>%
+        t %>%
+        as.data.frame %>%
+        arrange(red, green, blue)
+    with(r, rgb(red, green, blue, maxColorValue = 255))
+}
+    
