8f6fb725f10a808146b4c53c0aa6735b0fc98cb2
Dianne Cook <visnut@gmail.com>
2017-10-26 11:27:26 +1100

    mcrea

--- R/colors.R
+++ R/colors.R
@@ -19,3 +19,20 @@ namatjira <- c(
     "#c09060",
     "#f0a860"
 )
+
+## Georgiana McCrae, "Melbourne, Collins St, 1841",
+## https://en.wikipedia.org/wiki/File:Melbourne_Collins_Street_1841.jpg
+mccrea_melbcollinsst_1841 <- c(
+    "#a8c0a8",
+    "#c0c0a8",
+    "#a8a890",
+    "#909078",
+    "#a8a8a8",
+    "#c0c0c0",
+    "#d8c0a8",
+    "#c09048",
+    "#c07848",
+    "#d89060",
+    "#c07848"
+)
+    
