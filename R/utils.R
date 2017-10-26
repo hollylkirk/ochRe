7da1d05b44ef16eba4d20e15de7810d1df7ec6f2
Roger D. Peng [audrey] <rdpeng@gmail.com>
2017-10-26 15:55:53 +1100

    Import colorRampPalette

--- R/utils.R
+++ R/utils.R
@@ -5,6 +5,7 @@
 #' @param num numeric, the number of colors to use
 #' @export
 #' @importFrom graphics image
+#' @importFrom grDevices colorRampPalette
 viz_palette <- function(obj, nam, num = length(obj)) {
     pal_func <- colorRampPalette(obj)
     image(1:num, 1, as.matrix(1:num), col=pal_func(num), 
