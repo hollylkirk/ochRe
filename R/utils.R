e176d6a2d3cae26d48954aefad54a69fa6337c44
Roger D. Peng [audrey] <rdpeng@gmail.com>
2017-10-26 16:04:06 +1100

    Add an error check

--- R/utils.R
+++ R/utils.R
@@ -7,8 +7,10 @@
 #' @importFrom graphics image
 #' @importFrom grDevices colorRampPalette
 viz_palette <- function(obj, nam, num = length(obj)) {
+    if(num <= 0)
+        stop("'num' should be > 0")
     pal_func <- colorRampPalette(obj)
-    image(1:num, 1, as.matrix(1:num), col=pal_func(num), 
+    image(seq_len(num), 1, as.matrix(seq_len(num)), col = pal_func(num), 
           main = nam, ylab = "", xaxt = "n", yaxt = "n",  bty = "n",
           xlab = "")
 }
