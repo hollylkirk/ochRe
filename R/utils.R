2334f9340467ae504d081dedf2cf301185fdfb69
Roger D. Peng [audrey] <rdpeng@gmail.com>
2017-10-26 15:23:43 +1100

    Document 'nam' parameter

--- R/utils.R
+++ R/utils.R
@@ -1,11 +1,13 @@
 #' Show an ochre palette
 #'
 #' @param pal character, vector of (hexadecimal) colors characterizing a palette
+#' @param nam character, the name of the palette
 #' @export
 #' @importFrom graphics plot par text
 viz_palette <- function(obj, nam) {
     image(1:length(obj), 1, as.matrix(1:length(obj)), col=obj, 
-          main = nam, ylab = "", xaxt = "n", yaxt = "n",  bty = "n")}
+          main = nam, ylab = "", xaxt = "n", yaxt = "n",  bty = "n")
+}
 
 #' Order an ochre palette automatically
 #'
