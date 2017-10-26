ff911cb43085eed2ad3fb34bf6a6f09a917884a8
Roger D. Peng [audrey] <rdpeng@gmail.com>
2017-10-26 15:51:50 +1100

    Add number argument to viz_palette

--- R/utils.R
+++ R/utils.R
@@ -2,10 +2,12 @@
 #'
 #' @param obj character, vector of (hexadecimal) colors characterizing a palette
 #' @param nam character, the name of the palette
+#' @param num numeric, the number of colors to use
 #' @export
 #' @importFrom graphics image
-viz_palette <- function(obj, nam) {
-    image(1:length(obj), 1, as.matrix(1:length(obj)), col=obj, 
+viz_palette <- function(obj, nam, num = length(obj)) {
+    pal_func <- colorRampPalette(obj)
+    image(1:num, 1, as.matrix(1:num), col=pal_func(num), 
           main = nam, ylab = "", xaxt = "n", yaxt = "n",  bty = "n",
           xlab = "")
 }
