062f080ec897b1094dc5fe263f9b18e7dfb1f2b5
Roger D. Peng [audrey] <rdpeng@gmail.com>
2017-10-26 11:51:35 +1100

    Remove final comma at end of Tasmania palette

--- R/colors.R
+++ R/colors.R
@@ -48,7 +48,7 @@ tasmania <- c(
     "#6090d8", # wineglass bay blue
     "#c0c0d8", # crater lake grey
     "#183018", # fern green
-    "#486030", # enchanted walk moss
+    "#486030" # enchanted walk moss
 )
 
 ## Sydney Nolan, "Ned Kelly 1946",
