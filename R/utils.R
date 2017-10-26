8f3f45307e9ffe891a4fa35db86d745e854d9862
Roger D. Peng [audrey] <rdpeng@gmail.com>
2017-10-26 15:37:51 +1100

    import image from graphics; fix param name

--- R/utils.R
+++ R/utils.R
@@ -1,9 +1,9 @@
 #' Show an ochre palette
 #'
-#' @param pal character, vector of (hexadecimal) colors characterizing a palette
+#' @param obj character, vector of (hexadecimal) colors characterizing a palette
 #' @param nam character, the name of the palette
 #' @export
-#' @importFrom graphics plot par text
+#' @importFrom graphics image
 viz_palette <- function(obj, nam) {
     image(1:length(obj), 1, as.matrix(1:length(obj)), col=obj, 
           main = nam, ylab = "", xaxt = "n", yaxt = "n",  bty = "n",
