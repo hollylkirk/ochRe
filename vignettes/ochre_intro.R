3f390880f4a01887ab12e7d1c74b96d49b048278
Roger D. Peng [audrey] <rdpeng@gmail.com>
2017-10-26 16:53:55 +1100

    Increase figure height for palettes

---
+++ vignettes/ochre_intro.R
@@ -0,0 +1,14 @@
+## ----use_names-----------------------------------------------------------
+library(ochRe)
+
+# See names of all palettes
+names(ochre_palettes)
+
+## ----see_palettes--------------------------------------------------------
+pal_names <- names(ochre_palettes)
+
+par(mfrow=c(11, 1), lheight = 2, mar=rep(1, 4), adj = 0)
+for (i in 1:length(ochre_palettes)){
+    viz_palette(ochre_palettes[[i]], pal_names[i])
+}
+
